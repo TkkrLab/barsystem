c1a0f1eb70f343d16831113349997b9daad663a5
Jasper Seidel <lenny@jawsper.nl>
2015-08-19 00:40:46 +0200

    launcher.py: graceful exit on SIGTERM

--- init/launcher.py
+++ init/launcher.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 import os
+import signal
 import subprocess
+import sys
+
+def sigterm_handler(_signo, _stackframe):
+	sys.exit(0)
 
 def main(argv):
+	signal.signal(signal.SIGTERM, sigterm_handler)
+
 	os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 	with open('bin/activate_this.py') as f:
@@ -20,5 +27,4 @@ def main(argv):
 	return 0
 
 if __name__ == '__main__':
-	import sys
 	sys.exit(main(sys.argv))
