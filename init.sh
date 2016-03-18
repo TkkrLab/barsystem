71beeade7b463ae16b6a88b14b0be39c6bf97f72
Jasper Seidel <lenny@jawsper.nl>
2016-03-18 22:57:50 +0100

    Improve init.sh (still terrible)

--- init.sh
+++ init.sh
@@ -3,46 +3,58 @@
 DIR=$(dirname $(readlink -f $0))
 cd $DIR
 
-if [[ ! -e "$DIR/bin/activate" ]]; then
-	echo "Creating virtualenv"
-	virtualenv -p python3 . || exit -1
-fi
-
-echo "Activating virtualenv"
-source bin/activate || exit -2
-
-echo "Installing requirements"
-pip install -r requirements.txt || exit -3
-
-if [[ ! -x "$DIR/manage.py" ]]; then
-	echo "Generating default config"
-	django-admin startproject --template=init --name=nginx_params barsystem . || exit -4
-fi
-
-echo "Creating database and static dir"
-./manage.py migrate
-./manage.py collectstatic --noinput
-./manage.py compilemessages
-echo "Creating superuser"
-./manage.py createsuperuser
-
-NGINX_CONF_PATH=`nginx -V 2>&1 | grep -e "--prefix=" | tr ' ' '\n' | grep "^--prefix=" | awk -F= '{print $2}'`
-
-if [[ -z $NGINX_CONF_PATH ]]; then
-	NGINX_CONF_PATH="/usr/local/nginx/conf"
-	if [[ ! -d $NGINX_CONF_PATH ]]; then
-		echo "Cannot detect nginx config path!"
-		exit -5
+function create_virtualenv {
+	if [[ ! -e "$DIR/bin/activate" ]]; then
+		echo "Creating virtualenv"
+		virtualenv -p python3 . || exit -1
 	fi
-fi
+}
 
-if [[ ! -e $NGINX_CONF_PATH/barsystem_params ]]; then
-	echo "Linking server config to $NGINX_CONF_PATH/barsystem.conf"
-	sudo ln -s $DIR/server_config/nginx_params $NGINX_CONF_PATH/barsystem_params
-fi
-echo "Just add \"include barsystem_params\" in a \"server { }\" block to $NGINX_CONF_PATH/nginx.conf and restart nginx."
-# echo "After that launch launcher.sh (preferably on boot), and the bar should run."
+function activate_virtualenv {
+	echo "Activating virtualenv"
+	source bin/activate || exit -2
+}
 
+function install_requirements {
+	echo "Installing requirements"
+	pip install -r requirements.txt || exit -3
+}
+
+function django_init {
+	if [[ ! -x "$DIR/manage.py" ]]; then
+		echo "Generating default config"
+		django-admin startproject --template=init --name=nginx_params barsystem . || exit -4
+	fi
+}
+
+function barsystem_init {
+	echo "Creating database and static dir"
+	./manage.py migrate
+	./manage.py collectstatic --noinput
+	./manage.py compilemessages
+	echo "Creating superuser"
+	./manage.py createsuperuser
+}
+
+function nginx_config {
+	NGINX_CONF_PATH=`nginx -V 2>&1 | grep -e "--prefix=" | tr ' ' '\n' | grep "^--prefix=" | awk -F= '{print $2}'`
+
+	if [[ -z $NGINX_CONF_PATH ]]; then
+		NGINX_CONF_PATH="/usr/local/nginx/conf"
+		if [[ ! -d $NGINX_CONF_PATH ]]; then
+			echo "Cannot detect nginx config path!"
+			exit -5
+		fi
+	fi
+
+	if [[ ! -e $NGINX_CONF_PATH/barsystem_params ]]; then
+		echo "Linking server config to $NGINX_CONF_PATH/barsystem.conf"
+		sudo ln -s $DIR/server_config/nginx_params $NGINX_CONF_PATH/barsystem_params
+	fi
+	echo "Just add \"include barsystem_params\" in a \"server { }\" block to $NGINX_CONF_PATH/nginx.conf and restart nginx."
+}
+
+function create_init_script {
 # http://unix.stackexchange.com/questions/18209/detect-init-system-using-the-shell/164092#164092
 if [[ `systemctl` =~ -\.mount ]]; then
 	# systemd
@@ -157,3 +169,13 @@ EOF2
 else
 	echo "Cannot determine init system in use."
 fi
+
+}
+
+create_virtualenv
+activate_virtualenv
+install_requirements
+django_init
+barsystem_init
+nginx_config
+create_init_script
